5c24187e61c90f4727f3ccaafb0b21905e33647b
HESLS002 <Sam.Heslop@disney.com>
2014-02-21 17:20:51 +0000

    Finished IN queries and added support for gt, lt and contains queries. Close #2.

--- test/openData.test.js
+++ test/openData.test.js
@@ -1,7 +1,7 @@
 var should = require('should'),
 	oData = require('../lib/openData').OpenData;
 
-describe('createInParam Should Turn In Query Parameters into In Query Object', function(){
+describe('inObject Should Turn a Query Parameter into In Query Object', function(){
 	it('should', function(){
 
 		var requestParams = {
@@ -14,5 +14,32 @@ describe('createInParam Should Turn In Query Parameters into In Query Object', f
 		
 		requestParams.PropertyThree.$in[0].should.be.eql('ValueOne');
 		requestParams.PropertyThree.$in[1].should.be.eql('ValueTwo');
+	});
+});
+
+describe('gtObject Should return Greater Than Query Parameter into a Greater Than Query Object', function(){
+	it('should', function(){
+
+		var result = oData.gtObject('Value');
+		
+		result.should.be.eql({ '$gt' : 'Value' });
+	});
+});
+
+describe('ltObject Should return Less Than Query Parameter into a Less Than Query Object', function(){
+	it('should', function(){
+
+		var result = oData.ltObject('Value');
+		
+		result.should.be.eql({ '$lt' : 'Value' });
+	});
+});
+
+describe('containsObject Should return a Contains Parameter into a Regex Query Object', function(){
+	it('should', function(){
+
+		var result = oData.containsObject('mi');
+		
+		result.should.be.eql({ '$regex' : '.*mi.*' });
 	});
 });
