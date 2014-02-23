d82860a90519d1cf83939c84639f9e6a13b11138
SamHeslop <sam.heslop.dev@gmail.com>
2014-02-23 00:08:53 +0000

    Replace Restify with Express to allow serving a landing page.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var restify = require('restify'),
+var express = require('express'),
 	routes = require('./lib/routes'),
 	mongo = require('./lib/mongoParser');
 
-var app = restify.createServer();
-app.use(restify.queryParser());
+var app = express();
+
 app.use(mongo.mongoQueryParser);
 
 routes.exposeEndPoints(app);
+app.use(app.router);
+
+// app.use(express.static(__dirname + '/public/'));
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+// app.set('port', process.env.PORT || 3000);
 
 app.listen(3000, function(){
   console.log('API live at', app.url);
